c324200cad84cde8f5e54ddab96f4bae0adc48fe
Codeur47 <yoropapers@gmail.com>
2021-09-05 10:55:05 +0000

    Generate project structure

---
+++ src/test/java/com/yorosoft/eexpenseapi/EExpenseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yorosoft.eexpenseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EExpenseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
