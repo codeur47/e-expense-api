8c03d1cd782afd7ea5c367a486fe396ebe526fd4
Codeur47 <yoropapers@gmail.com>
2021-09-05 21:46:45 +0000

    Feat(#6feat-Implements_Exceptions) : Implements Controllers, enable Swagger

---
+++ src/main/java/com/yorosoft/eexpenseapi/config/CrudControllerAPIPath.java
@@ -0,0 +1,15 @@
+package com.yorosoft.eexpenseapi.config;
+
+public class CrudControllerAPIPath {
+
+    public static final String CATEGORY_BASE_URL = "/category";
+    public static final String PURCHASE_BASE_URL = "/purchase";
+    public static final String GET_ALL = "/";
+    public static final String GET_BY_ID = "/{id}";
+    public static final String SAVE = "/";
+    public static final String DELETE_BY_ID = "/{id}";
+    public static final String UPDATE_BY_ID = "/{id}";
+
+    private CrudControllerAPIPath() {
+    }
+}
