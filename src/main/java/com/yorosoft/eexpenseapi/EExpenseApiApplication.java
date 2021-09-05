0fae62676ea2d4c0b4ed3889ea1612d51113c7eb
Codeur47 <yoropapers@gmail.com>
2021-09-05 11:39:57 +0000

    Update EExpenseApiApplication.java

--- src/main/java/com/yorosoft/eexpenseapi/EExpenseApiApplication.java
+++ src/main/java/com/yorosoft/eexpenseapi/EExpenseApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EExpenseApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EExpenseApiApplication.class, args);
 	}
-
 }
